cedfef5dd3c667553af693369b748b3fd6bf8a9c
dracula109 <akhilkgangadharan109@gmail.com>
2017-09-28 12:32:51 +0530

    updateRange function added

--- segment_trees_1.cpp
+++ segment_trees_1.cpp
@@ -2,13 +2,6 @@
 
 using namespace std;
 
-int gcd(int a,int b){
-    if(b == 0){
-        return a;
-    }
-    return gcd(b,a%b);
-}
-
 void build(int tree[],int A[],int node,int s,int e){
     if(s == e){
         tree[node] = A[s];
@@ -20,11 +13,19 @@ void build(int tree[],int A[],int node,int s,int e){
     }
 }
 
-/*void update(int tree[],int A[],int node,int s,int e){
-    if(s == e){
-        A
+void updateRange(int tree[],int A[],int node,int s,int e,int l,int r,int val){
+    if(s>e || r<s || e<l){
+        return;
     }
-}*/
+    if(s == e){
+        tree[node] += val;
+        return;
+    }
+    int mid = (s+e)/2;
+    updateRange(tree,A,2*node,s,mid,l,r,val);
+    updateRange(tree,A,2*node+1,mid+1,e,l,r,val);
+    tree[node] = tree[2*node] + tree[2*node+1];
+}
 
 int query(int tree[],int A[],int node,int s,int e,int l,int r){
     if(r < s or e < l){
@@ -45,10 +46,22 @@ int n = sizeof(A)/sizeof(int);
 int tree[2*n];
 build(tree,A,1,0,n-1);
 int q,x,y;
-cin>>q;
-for(int i=0;i<q;i++){
-    cin>>x>>y;
-    cout<<query(tree,A,1,0,n-1,x,y);
+
+/* cin>>q;
+   for(int i=0;i<q;i++){
+        cin>>x>>y;
+        cout<<query(tree,A,1,0,n-1,x,y);
+    }
+*/
+
+for(int i=0;i<2*n;i++){
+    cout<<tree[i]<<" ";
 }
+cout<<endl;
+updateRange(tree,A,1,0,n-1,2,5,1);
+for(int i=0;i<2*n;i++){
+    cout<<tree[i]<<" ";
+}
+cout<<endl;
 return 0;
 }
